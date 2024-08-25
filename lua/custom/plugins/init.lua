a6427e01c311e1930f95992b80f681b0b74a9499
itsChoudhry <23042676+ItsChoudhry@users.noreply.github.com>
2024-08-25 12:56:16 +0100

    remove grep thing in place of kickstart grep thing (sg)

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -18,8 +18,5 @@ vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
 vim.keymap.set('n', '<leader>Y', [["+Y]])
 vim.keymap.set('n', 'Q', '<nop>')
 vim.keymap.set('n', '<C-f>', '<cmd>silent !tmux neww tmux-sessionizer<CR>')
-vim.keymap.set('n', '<leader>ps', function()
-  builtin.grep_string { search = vim.fn.input 'Grep > ' }
-end)
 
 return M
