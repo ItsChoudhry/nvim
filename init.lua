4f30beeb70e532610d11d38f6a56513572374666
itschoudhry <23042676+ItsChoudhry@users.noreply.github.com>
2025-05-06 15:47:09 +0100

    fix theme setup

--- init.lua
+++ init.lua
@@ -744,21 +744,26 @@ require('lazy').setup({
       }
     end,
   },
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  {
     'rebelot/kanagawa.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'kanagawa-dragon'
+    lazy = false, -- Load immediately to ensure colorscheme is available
+    priority = 1000, -- High priority to load before other plugins
+    config = function()
+      -- Optional: Configure Kanagawa before setting the colorscheme
+      require('kanagawa').setup {
+        -- Customize options here (see below for examples)
+        theme = 'dragon', -- Options: "wave", "dragon", "lotus"
+        background = { -- Set background for dark/light modes
+          dark = 'dragon',
+          light = 'lotus',
+        },
+        transparent = true, -- Set to true for transparent background
+        terminalColors = true,
+        -- keywordStyle = { italic = false },
+        -- statementStyle = { bold = false },
+      }
 
-      vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+      vim.cmd 'colorscheme kanagawa'
     end,
   },
 
